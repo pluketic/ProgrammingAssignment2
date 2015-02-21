05e932fa591236aa5cf9c05f207bb9823e9ef176
pluketic <luketic.petar@gmail.com>
2015-02-21 21:30:59 +0200

    Comments update

--- cachematrix.R
+++ cachematrix.R
@@ -9,15 +9,15 @@
 ##          - setinverse: sets the inverse of the matrix to be cached
 
 makeCacheMatrix <- function(x = matrix()) {
-    inverse_x <- NULL                                   # sets the value of local vector inverse_x to NULL (placeholder for inverse matrix)
-    set<-function(y){                                   # define set() function which 
+    inverse_x <- NULL                                   # sets the value of local object inverse_x to NULL (placeholder for inverse matrix)
+    set<-function(y){                                   # defines set() function which 
         x <<- y                                         # caches input (new) matrix
-        inverse_x <<- NULL                              # sets the value of cached vector inverse_x to NULL
+        inverse_x <<- NULL                              # sets the value of cached object inverse_x to NULL
     }
-    get<-function() x                                   # define get() function which returns cached matrix
-    setinverse<-function(solve) inverse_x <<- solve     # define setmatrix() function which caches variable m and assigns to it inverse of the input matrix
-    getinverse<-function() inverse_x                    # define getmatrix() function which returns cached variable inverse_x (inverse of the input matrix)
-    list(set=set, get=get,                              # define the list of external functions and their local definition names
+    get<-function() x                                   # defines get() function which returns cached matrix
+    setinverse<-function(solve) inverse_x <<- solve     # defines setmatrix() function which caches object inverse_x and assigns to it inverse of the input matrix
+    getinverse<-function() inverse_x                    # defines getmatrix() function which returns cached object inverse_x (inverse of the input matrix)
+    list(set=set, get=get,                              # defines the list of functions and their local definition names
          setinverse=setinverse,
          getinverse=getinverse)
 }
@@ -28,14 +28,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
-    inverse_x <- x$getinverse()                         # defines local vector inverse_x and assigns it a cached vector (inverse of the input matrix)
+    inverse_x <- x$getinverse()                         # defines local object inverse_x and assigns it a cached object (inverse of the input matrix)
     if(!is.null(inverse_x)){                            # if there is an cached inverse matrix inverse_x available in the cache
-        print("getting cached inverse matrix")          # notify me about existing cached vector
-        return(inverse_x)                               # and make function return cached vector inverse_x (inverse of the input matrix)
+        print("getting cached inverse matrix")          # notify me about existing cached object
+        return(inverse_x)                               # and make function return cached object inverse_x (inverse of the input matrix)
     } else {                                            # else
         print("getting new data")                       # notify me that the is no cached data available, so that the function has to make inverse matrix from scratch
         matrix <- x$get()                               # run the get() function to get the input matrix
-        inverse_x <- solve(matrix, ...)                 # define local variable inverse_x and assign it a value of inverse of the input matrix
+        inverse_x <- solve(matrix, ...)                 # define local object inverse_x and assign it a value of inverse of the input matrix
         x$setinverse(inverse_x)                         # run the setmatrix function on the input matrix to cache it
         return(inverse_x)                               # return inverse_x
     }
