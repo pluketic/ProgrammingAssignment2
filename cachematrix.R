0c8339dcaa69baef863c26e0d44e06f5b9db4d67
pluketic <luketic.petar@gmail.com>
2015-02-21 20:27:13 +0200

    "Updated comments"

--- cachematrix.R
+++ cachematrix.R
@@ -6,14 +6,14 @@
 ## makeCacheMatrix function defines methods for inversing & caching inverse of the input matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-    inverse_x <- NULL                                   # sets the value of local vector m to NULL (placeholder for inverse matrix)
+    inverse_x <- NULL                                   # sets the value of local vector inverse_x to NULL (placeholder for inverse matrix)
     set<-function(y){                                   # define set() function which 
         x <<- y                                         # caches input matrix
-        inverse_x <<- NULL                              # sets the value of cached variable m to NULL
+        inverse_x <<- NULL                              # sets the value of cached vector inverse_x to NULL
     }
     get<-function() x                                   # define get() function which returns input matrix
     setinverse<-function(solve) inverse_x <<- solve     # define setmatrix() function which caches variable m and assigns to it inverse of the input matrix
-    getinverse<-function() inverse_x                    # define getmatrix() function which returns cached variable m (inverse of the input matrix)
+    getinverse<-function() inverse_x                    # define getmatrix() function which returns cached variable inverse_x (inverse of the input matrix)
     list(set=set, get=get,                              # define the list of external functions and their local definition names
          setinverse=setinverse,
          getinverse=getinverse)
@@ -24,16 +24,16 @@ makeCacheMatrix <- function(x = matrix()) {
 ## inverse matrix already exists in cache, if not then it calculates it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-    inverse_x <- x$getinverse()                         # define local vector m and assign it cached vector m (inverse of the input matrix)
-    if(!is.null(inverse_x)){                            # if there is an cached inverse matrix m available in the cache
+    ## Return a matrix that is the inverse of 'x'
+    inverse_x <- x$getinverse()                         # defines local vector inverse_x and assigns it a cached vector (inverse of the input matrix)
+    if(!is.null(inverse_x)){                            # if there is an cached inverse matrix inverse_x available in the cache
         print("getting cached inverse matrix")          # notify me about existing cached vector
-        return(inverse_x)                               # and make function return cached vector m (inverse of the input matrix)
+        return(inverse_x)                               # and make function return cached vector inverse_x (inverse of the input matrix)
     } else {                                            # else
         print("getting new data")                       # notify me that the is no cached data available, so that the function has to make inverse matrix from scratch
         matrix <- x$get()                               # run the get() function to get the input matrix
-        inverse_x <- solve(matrix, ...)                 # define local variable m and assign it a value of inverse of the input matrix
+        inverse_x <- solve(matrix, ...)                 # define local variable inverse_x and assign it a value of inverse of the input matrix
         x$setinverse(inverse_x)                         # run the setmatrix function on the input matrix to cache it
-        return(inverse_x)   
+        return(inverse_x)                               # return inverse_x
     }
 }
